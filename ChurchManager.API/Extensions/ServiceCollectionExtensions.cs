2f98abe0cec28d374f29eb6ee16029a298898dd2
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2021-07-30 20:24:39 -0300

    startup com jwt

---
+++ ChurchManager.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using ChurchManager.Application.Commands.AddIgreja;
+using ChurchManager.Application.Commands.AddMembro;
+using ChurchManager.Application.Commands.AddUsuario;
+using ChurchManager.Application.Queries.GetIgrejas;
+using ChurchManager.Application.Queries.GetMembros;
+using ChurchManager.Application.Servicos;
+using ChurchManager.Infrastructure.Persistencia.UnitOfWork;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChurchManager.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, ChurchManager.Infrastructure.Persistencia.UnitOfWork.UnitOfWork>();
+            services.AddScoped<IUnitOfWorkLicenciado, ChurchManager.Infrastructure.Persistencia.UnitOfWork.UnitOfWorkLicenciado>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            //Services
+            services.AddScoped<IMembroService, ChurchManager.Application.Servicos.MembroService>();
+            services.AddScoped<IIgrejaService, ChurchManager.Application.Servicos.IgrejaService>();
+
+            //Commands
+            services.AddMediatR(typeof(AddMembroCommand));
+            services.AddMediatR(typeof(AddIgrejaCommand));
+
+            //Queries
+            services.AddMediatR(typeof(GetMembrosQuery));
+            services.AddMediatR(typeof(GetIgrejasQuery));
+
+            return services;
+        }
+
+    }
+}
--- ChurchManager.API/Startup.cs
+++ ChurchManager.API/Startup.cs
@@ -1,14 +1,22 @@
+using ChurchManager.API.Extensions;
+using ChurchManager.Domain.Settings;
+using ChurchManager.Infrastructure.Persistencia;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ChurchManager.API
@@ -25,7 +33,43 @@ namespace ChurchManager.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddDbContext<CmDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")))
+                .AddDbContext<IdDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")))
+                .AddDbContext<LicenciadosDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LicenciadosConnection")))
+                .AddInfrastructure()
+                .AddApplication()
+                .AddControllers();
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddDefaultTokenProviders()
+                    .AddEntityFrameworkStores<IdDbContext>();
+
+            //JWT
+            var appSettingsSection = Configuration.GetSection("Setting");
+            services.Configure<Setting>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<Setting>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
