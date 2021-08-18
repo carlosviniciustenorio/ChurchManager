3035b4bfbebed674f053c8fc34bbc3378d5fd847
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2021-08-18 08:29:23 -0300

    adicionando teste de entidade da Igreja

---
+++ ChurchManager.Domain.Tests/IgrejaTest/IgrejaTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoFixture;
+using ChurchManager.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChurchManager.Domain.Tests.IgrejaTest
+{
+    public class IgrejaTests
+    {
+        [Fact]
+        public void AdicionandoDirigenteNaIgrejaComSucesso()
+        {
+            //Arrange
+            var fixture = new Fixture();
+            var igreja = fixture.Create<Igreja>();
+
+            //Act
+            igreja.AdicionarDirigente(1);
+
+            //Assert
+            Assert.True(igreja.DirigenteId == 1);
+        }
+    }
+}
