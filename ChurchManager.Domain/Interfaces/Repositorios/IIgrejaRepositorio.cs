2dce6f263c061040134f5312f3137c1811ab17d3
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2021-08-22 11:50:24 -0300

    add methods in IIgrejaRepositorio

--- ChurchManager.Domain/Interfaces/Repositorios/IIgrejaRepositorio.cs
+++ ChurchManager.Domain/Interfaces/Repositorios/IIgrejaRepositorio.cs
@@ -1,6 +1,8 @@
 ﻿using ChurchManager.Domain.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace ChurchManager.Domain.Interfaces.Repositorios
@@ -8,5 +10,11 @@ namespace ChurchManager.Domain.Interfaces.Repositorios
     public interface IIgrejaRepositorio
     {
         void Add(Igreja entity);
+        void Edit(Igreja entity);
+        void Remove(Igreja entity);
+        Igreja FindById(int id);
+        IQueryable<Igreja> FindBy(Expression<Func<Igreja, bool>> predicate);
+        IQueryable<Igreja> FindAll();
+        void Dispose();
     }
 }
